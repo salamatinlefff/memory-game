b9fcc301ec44541db14ac30742b954dda53444a1
Lev <salamatinlefff@gmail.com>
2022-02-22 15:42:11 +0400

    refactor: change sound

--- assets/scripts/music.js
+++ assets/scripts/music.js
@@ -1,20 +1,22 @@
 export const audio = {
-  background: new Audio(`./assets/audio/background2.mp3`),
+  background: new Audio(`./assets/audio/background1.mp3`),
   positiveResponse: new Audio(`./assets/audio/positive1.mp3`),
-  negativeResponse: new Audio(`./assets/audio/negative4.mp3`),
+  negativeResponse: new Audio(`./assets/audio/negative1.mp3`),
   victory: new Audio(`./assets/audio/victory.mp3`),
   click: new Audio('./assets/audio/click2-min.mp3'),
-  changeVolume: new Audio('./assets/audio/change_volume.mp3'),
 }
 
 export const music = () => {
   const inputBackground = document.querySelector('.overlay__input_background');
   const inputEffects = document.querySelector('.overlay__input_effects');
+  const inputIncorrect = document.querySelector('.overlay__input_incorrect');
   const muteBackground = document.getElementById('mute');
 
   inputBackground.style.background = `linear-gradient(to right, #e78300 ${inputBackground.value}%, #ffffff ${inputBackground.value}%`;
 
   inputEffects.style.background = `linear-gradient(to right, #e78300 ${inputEffects.value}%, #ffffff ${inputEffects.value}%`;
+
+  inputIncorrect.style.background = `linear-gradient(to right, #e78300 ${inputIncorrect.value}%, #ffffff ${inputIncorrect.value}%`;
 
   let backgroundIsPlaying = false;
   
@@ -47,10 +49,16 @@ export const music = () => {
     inputEffects.style.background = `linear-gradient(to right, #e78300 ${inputEffects.value}%, #ffffff ${inputEffects.value}%`;
     
     audio.click.volume = inputEffects.value / 100;
-    audio.changeVolume.volume = inputEffects.value / 100;
     audio.victory.volume = inputEffects.value / 100;
     audio.positiveResponse.volume = inputEffects.value / 100;
-    audio.negativeResponse.volume = inputEffects.value / 100;
+  })
+
+  inputIncorrect.addEventListener('input', () => {
+    audio.negativeResponse.play();
+
+    inputIncorrect.style.background = `linear-gradient(to right, #e78300 ${inputIncorrect.value}%, #ffffff ${inputIncorrect.value}%`;
+    
+    audio.negativeResponse.volume = inputIncorrect.value / 100;
   })
 
   muteBackground.addEventListener('change', () => {
